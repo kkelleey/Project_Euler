99a08dacc64a2ba6be5d09003549edfd0be1715e
Karen Kelley <karenkelley11@gmail.com>
2016-06-02 21:29:20 -0500

    Making solution to problem_9 more OO

--- ruby/problem_9.rb
+++ ruby/problem_9.rb
@@ -1,21 +1,30 @@
-#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-#Find the product abc.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
 
-a = 0 
-found = false
+class PythagoreanTriple
+  def initialize(sum)
+    @sum = sum
+  end
 
-until found
-	a += 1
-	b = a + 1
-	until b == (1000 - a)/2
-		c = 1000 - b - a
-		if a ** 2 + b ** 2 == c ** 2
-			found = true
-			break
-		end
-		b += 1
-	end
+  def a_b_c
+    a = 1
+    loop do
+      a += 1
+      b = a + 1
+      until b == (1000 - a) / 2
+        c = 1000 - b - a
+        return [a,b,c] if pythagorean_triple?(a, b, c)
+        b += 1
+      end
+    end
+  end
+
+  def pythagorean_triple?(a, b, c)
+    a**2 + b**2 == c**2
+  end
 end
 
-puts "a = #{a}, b = #{b}, c = #{c}"
-puts "product = "+ (a * b * c).to_s
+# puts "a = #{a}, b = #{b}, c = #{c}"
+triple = PythagoreanTriple.new(1000)
+a_b_c = triple.a_b_c
+puts "product = " + a_b_c.reduce(:*).to_s
