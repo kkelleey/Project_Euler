0073f787170a1ceace6e654558a4c4aaa43150a3
Karen Kelley <karenkelley11@gmail.com>
2016-06-07 20:57:08 -0500

    Cleaning up problem_4

--- ruby/problem_2.rb
+++ ruby/problem_2.rb
@@ -1,15 +1,15 @@
-#y considering the terms in the Fibonacci sequence whose values do not exceed four million, 
-#find the sum of the even-valued terms.
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+# find the sum of the even-valued terms.
 
 last_num = 1
 second_to_last_num = 1
 sum = 0
 
 while last_num < 4000000
-	new_fib = last_num + second_to_last_num
-	second_to_last_num = last_num
-	last_num = new_fib
-	sum += new_fib if new_fib % 2 == 0
+  new_fib = last_num + second_to_last_num
+  second_to_last_num = last_num
+  last_num = new_fib
+  sum += new_fib if new_fib % 2 == 0
 end
 
 puts sum
--- ruby/problem_4.rb
+++ ruby/problem_4.rb
@@ -1,30 +1,25 @@
-#Problem 4 
+# Problem 4 
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-#Find the largest palindrome made from the product of two 3-digit numbers.
-upper_bound = 999 * 999
+# Find the largest palindrome made from the product of two 3-digit numbers.
 
+upper_bound = 999 * 999
 i = upper_bound
 
-
-
-def is_palindromic(i)
-	string = i.to_s
-	string == string.reverse
+def palindromic?(i)
+  i.to_s == i.to_s.reverse
 end
 
 def product_3_digit(num)
-	(100..999).to_a.each do |i|
-		if num % i == 0 && (num / i).to_s.length == 3
-			return true
-		end
-	end
-	false
+  (100..999).to_a.each do |i|
+    return true if num % i == 0 && (num / i).to_s.length == 3
+  end
+  false
 end
 
-until i == 0
-	if is_palindromic(i) && product_3_digit(i)
-		puts i
-		break
-	end
-	i -= 1
+loop do
+  if palindromic?(i) && product_3_digit(i)
+    puts i
+    break
+  end
+  i -= 1
 end
