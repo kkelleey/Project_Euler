f1cf6916a7d8ad97eec7cc490fe984dda8e7557b
Karen Kelley <karenkelley11@gmail.com>
2016-06-07 20:34:23 -0500

    Making problem_1 more OO

--- ruby/problem_1.rb
+++ ruby/problem_1.rb
@@ -1,8 +1,24 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
-for num in 1...1000
-  sum += num if num % 3 == 0 || num % 5 ==0
-  puts num
+class SumCalculator
+  def initialize(number1, number2, upper_bound)
+    @number1 = number1
+    @number2 = number2
+    @upper_bound = upper_bound
+  end
+
+  def sum_of_multiples_below_upper_bound
+    lcm = @number1.lcm(@number2)
+    sum_of_multiples_of(@number1) + sum_of_multiples_of(@number2) -
+      sum_of_multiples_of(lcm)
+  end
+
+  private
+
+  def sum_of_multiples_of(n)
+    modified_bound = @upper_bound / n
+    (1..modified_bound).reduce(0) { |a, e| a + n * e }
+  end
 end
-puts sum
+
+puts SumCalculator.new(3, 5, 1000).sum_of_multiples_below_upper_bound
