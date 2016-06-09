1838d201a259cee5e1e9a85e8b3bb98f22f07bd9
Karen Kelley <karenkelley11@gmail.com>
2016-06-09 12:55:05 -0500

    Adding Bignum#digts

--- ruby/problem_16.rb
+++ ruby/problem_16.rb
@@ -1,3 +1,9 @@
 # What is the sum of the digits of the number 2**1000?
 
-puts (2**1000).to_s.chars.map(&:to_i).reduce(:+)
+class Bignum
+  def digits
+    to_s.chars.map(&:to_i)
+  end
+end
+
+puts (2**1000).digits.reduce(:+)
