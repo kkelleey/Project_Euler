65c39fa70749d6d72a3e556eb033d1044a88cdf7
Karen Kelley <karenkelley11@gmail.com>
2016-06-09 12:43:45 -0500

    Refactoring problem 15

--- ruby/problem_15.rb
+++ ruby/problem_15.rb
@@ -1,17 +1,13 @@
 #Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 # How many such routes are there through a 20×20 grid?
-n = 2
 
-# 2n choose n
+# 2n choose n = (2n)! / (n!)**2 for all n >= 0
 
-numerator = 1
+class Fixnum
+  def !
+    (1..self).reduce(:*)
+  end
+end
 
-(2*n).downto(1) {|k| numerator *= k}
-
-
-denom = 1
-n.downto(1) {|k| denom *= k}
-
-answer = numerator / (denom **2)
-
-puts answer
+n = 20
+puts (2 * n).! / n.!**2
