8f6a7169e3f9df48e21ecb18fe70db3adacecb90
Karen Kelley <karenkelley11@gmail.com>
2016-06-08 23:40:30 -0500

    Refactoring problem 7

--- ruby/problem_7.rb
+++ ruby/problem_7.rb
@@ -1,17 +1,14 @@
-#What is the 10 001st prime number?
-primes = []
-i = 2
+# What is the 10 001st prime number?
+primes = [2]
+i = 3
 
-while primes.length < 10001
-	is_prime = true	
-	primes.each do |prime|
-		if i % prime == 0
-			is_prime = false
-			break
-		end
-	end
-	primes << i if is_prime
-	i += 1
+while primes.length <= 10001
+  if primes.any? { |p| i % p == 0 }
+    i += 2
+    next
+  end
+  primes << i
+  i += 2
 end
 
 puts primes.last
