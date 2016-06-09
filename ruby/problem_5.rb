07163d3301e96c81310d00e0d9b303005b40ebd1
Karen Kelley <karenkelley11@gmail.com>
2016-06-08 23:13:57 -0500

    Refactoring problem 5

--- ruby/problem_5.rb
+++ ruby/problem_5.rb
@@ -1,16 +1,7 @@
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-found = false
-i = 20
-
-until i == 20 * 19 * 18 * 17 * 15 * 14 * 13 * 12 * 11
-	all_are_divisors = true
-	(11..20).to_a.each do |j|
-		unless i % j == 0
-			all_are_divisors = false
-			break
-		end
-	end
-	break if all_are_divisors
-	i += 1
+lcm = (1..20).reduce(1) do |a, e|
+  e.lcm(a)
 end
+
+puts lcm
