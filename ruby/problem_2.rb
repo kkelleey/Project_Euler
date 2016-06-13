9f15e20f7df9996005e2ed9dca111b523f35b70f
Karen Kelley <karenkelley11@gmail.com>
2016-06-12 22:57:52 -0500

    Refactoring ruby problem 2

--- ruby/problem_2.rb
+++ ruby/problem_2.rb
@@ -5,11 +5,11 @@ last_num = 1
 second_to_last_num = 1
 sum = 0
 
-while last_num < 4000000
+while last_num < 4_000_000
   new_fib = last_num + second_to_last_num
   second_to_last_num = last_num
   last_num = new_fib
-  sum += new_fib if new_fib % 2 == 0
+  sum += new_fib if new_fib.even?
 end
 
 puts sum
